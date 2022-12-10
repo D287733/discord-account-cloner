4c0e6be22b14e8b2d5cc6ac67a5a449009c07c1f
D287733 <97526512+D287733@users.noreply.github.com>
2022-12-10 13:40:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,32 @@ header_invitation = {"Authorization": token, "Content-Type": 'application/json',
 
 payload = {}
 def guild_show():
-  x = 0
-  while True:
+  x = 0 # number guild 
+  
+
+  while True: # guild checking 
     try:
       guild_id = requests_info_guild[x]['id']
       print(f"[{Fore.GREEN}+{Fore.WHITE}]guild      : {guild_id}\t{requests_info_guild[x]['name']}")
-      
-      requests_for_channel_checker = requests.get(f'https://discord.com/api/v8/guilds/{guild_id}/channels', headers=header_old).json()
-      i = 0
-      while True: 
+
+
+      vanity = vanity_url_detect(guild_id) # func check vanity
+
+
+      requests_for_channel_checker = requests.get(f'https://discord.com/api/v8/guilds/{guild_id}/channels', headers=header_old).json() # info channel check type 
+
+      i = 0 # number channel 
+      while True: # channel  checking
+        if vanity == None:
+           pass
+        else:
+          url_server.append(f"discord.gg/{vanity}")
+          print(f"[{Fore.GREEN}+{Fore.WHITE}]invitation : {url_server[x]}")
+          print("\n")
+          x = x + 1
+          time.sleep(1) # slow mod optional
+          break
+        
         try: 
           if requests_for_channel_checker[i]['type'] == 0: 
             good_channel = requests_for_channel_checker[i]['id']
@@ -28,7 +45,7 @@ def guild_show():
             if requests_info_invitation['code'] == 50013:
               i = i + 1
         
-            else:
+            else: # code gen success
               url_server.append(f"discord.gg/{requests_info_invitation['code']}")
               print(f"[{Fore.GREEN}+{Fore.WHITE}]channel    : {good_channel}\t{requests_for_channel_checker[i]['name']}")
               print(f"[{Fore.GREEN}+{Fore.WHITE}]invitation : {url_server[x]}")
@@ -38,13 +55,19 @@ def guild_show():
               break
           else:
             i = i + 1 
-        except:
-          print(f"\n[{Fore.RED}-{Fore.WHITE}]Error")
+
+        except Exception:
+          print("error")
+          x = x + 1 
           break
+
 
     except:
       print(f"\n[{Fore.YELLOW}~{Fore.WHITE}]Step 1 Success!")
       break
+def vanity_url_detect(guild_id):
+  requests_info_guild_vanity = requests.get(f'https://discord.com/api/v9/guilds/{guild_id}', headers=header_old).json()
+  return requests_info_guild_vanity['vanity_url_code']
 
 
 def list_success_server():
@@ -52,12 +75,11 @@ def list_success_server():
     f = open("backup.txt", "a")
     v = 0
     for _ in requests_info_guild:
-        try:
-            url = int({url_server[v]})
-            url = "❌"
+      if url_server[v] == "30016": # soon update
+        url = "❌"
 
-        except:
-            url = "✔️"
+      else:
+        url = "✔️"
         print(f"guild  : {requests_info_guild[v]['name']}  : {url}")
         v = v + 1
         try:
